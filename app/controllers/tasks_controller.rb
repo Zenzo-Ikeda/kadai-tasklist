50233eb868939050372a60b59ab314152a519bb9
Zenzo.Ikeda <zz.Ikeda0707@gmail.com>
2020-02-25 12:44:55 +0000

    user-tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,8 @@ class TasksController < ApplicationController
   before_action :set_task , only:[:show,:edit,:update,:destroy]
 
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    #@tasks = Task.all.page(params[:page])
   end
   
   def show
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが登録されました'
